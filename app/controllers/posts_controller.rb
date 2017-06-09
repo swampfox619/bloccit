d8a8051f51fbd36ec04bc73d12b708d6f991daae
Lewis Vine <lew.vine@gmail.com>
2017-06-09 00:22:29 +0000

    checkpoint-20

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
     @posts.each do |post|
@@ -8,13 +9,16 @@ class PostsController < ApplicationController
     end
   end
 
+
   def show
     @post = Post.find(params[:id])
   end
 
+
   def new
     @post = Post.new
   end
+
 
   def create
     @post = Post.new
@@ -30,8 +34,38 @@ class PostsController < ApplicationController
     end
   end
   
-
-
+  
   def edit
+    @post = Post.find(params[:id])
   end
+  
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+ 
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+  
+  
+  def destroy
+    @post = Post.find(params[:id])
+ 
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
+ 
+  
 end
