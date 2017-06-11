65d145cb3376325fd936c453c8723ce1470525be
Lewis Vine <lew.vine@gmail.com>
2017-06-11 00:09:33 +0000

    assignment-23

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe User, type: :model do
             it "should have name and email attributes" do
                 expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
             end
+            
+            it "should capitalize both names" do
+                user.name = "bob jones"
+                user.save
+                expect(user.name).to eq("Bob Jones")
+            end
         end
    
         describe "invalid user" do
